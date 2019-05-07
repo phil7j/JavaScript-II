95f55a43fb48a9717c195e37270d6584a4d974b5
Philip Johnson <phil7j@gmail.com>
2019-05-07 14:08:39 -0700

    complete callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,11 +1,13 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work.
+//  You have been provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
-  //Given this problem: 
-  
+/*
+
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -17,34 +19,46 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     return cb(arr[0]);
   }
 
-  // Function invocation 
+  // Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
 
 */
-
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-}
+//  getLength passes the length of the array into the callback.
+  cb("Array Length is " + arr.length);
+};
+getLength(items, console.log);
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb("Last item is " + arr[arr.length-1]);
 }
+last(items, console.log);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 }
+sumNums(5,5, console.log);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y);
 }
+multiplyNums(5,2, console.log);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item))
 }
+contains('Gum', items, console.log);
 
 /* STRETCH PROBLEM */
 
