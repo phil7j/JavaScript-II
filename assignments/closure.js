bcf60171960842e86bb03305259f417b1ba2f831
Philip Johnson <phil7j@gmail.com>
2019-05-07 15:38:58 -0700

    complete MVP for all assignments

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,12 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function greetWorld(greeting){
+  let hello = greeting;
+  (function(){
+    console.log(`${hello} World!`);
+  })();
+}
+greetWorld("greetings");
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
